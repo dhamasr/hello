8dd01baee2251e5493d2a3c92a3da4ec0a2211f1
Dhamayanthi <dhamasr@gmail.com>
2015-07-25 15:02:59 +0530

    first commit of hello world for yii2

---
+++ views/site/say.php
@@ -0,0 +1,12 @@
+<!--/**
+* Created by PhpStorm.
+* User: kavin
+* Date: 25/7/15
+* Time: 2:28 PM
+*/-->
+<?php
+use yii\helpers\Html;
+?>
+    <h1>Hello <?= Html::encode($target) ?></h1>
+    <p>Welcome to your Yii2 demonstration application.</p>
+
